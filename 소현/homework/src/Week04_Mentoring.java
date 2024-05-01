b653825622fc627778724223704e3f5b197b7bf6
sohyuunii <psh9760@naver.com>
2024-05-02 03:37:07 +0900

    4주차 멘토링 과제 최종본

--- 소현/homework/src/Week04_Mentoring.java
+++ 소현/homework/src/Week04_Mentoring.java
@@ -8,36 +8,29 @@ public class Week04_Mentoring {
 
         // 성함 입력(문자열), inputMyName은 입력받은 값을 저장하는 변수
         System.out.print("성함을 입력해주세요 => ");
-        String inputMyName = scanner.nextLine().trim();
+        String inputMyName = scanner.nextLine().trim(); //trim() 메서드는 입력받은 문자열의 양 끝 공백 제거
 
         // 부원들의 이름 한 번에 입력받기, inputNames은 입력받은 값을 저장하는 변수
-        System.out.print("부원들의 이름을 입력해주세요 >> ");
-        String inputNames = scanner.nextLine().trim();
-
+        System.out.print("부원 5명의 이름을 입력해주세요 >> ");
         // 입력된 이름들을 리스트에 추가
         List<String> names = new ArrayList<>();
-        // split는 문자열을 특정 구분자를 기준으로 분리하는 메서드
-        // \\s+ 는 공백을 나타냄
-        // for문에서 split 메서드에 의해 분리된 이름을 name변수에 할당
-        for (String name : inputNames.split("\\s+")) {
+        // 5번을 반복하는 for문을 통해 부원들의 이름을 입력받음
+        for (int i = 0; i<5; i++){
+            String name = scanner.nextLine().trim(); //입력받은 이름을 name변수에 저장
             names.add(name); //names리스트에 각 이름들이 추가됨
         }
 
         scanner.close(); // Scanner 닫기
 
         // 본인과 같은 성을 가진 부원 검색 및 출력
-        String[] myNameParts = inputMyName.split("\\s+");
-        String myLastName = myNameParts[0]; // myLastName은 내 성 부분(인덱스 0번째)
-
-        // 본인과 같은 성을 가진 부원 검색 및 출력
         System.out.print("나와 성이 같은 사람은 ");
-        int count = 0;
-        for (String name : names) {
+        List<String> sameName = new ArrayList<>(); //같은 성을 가진 부원들의 이름을 저장할 sameName리스트 생성
+        int count = 0; //부원 수를 세기 위한 변수 count
+        for (String name : names) { //리스트 name에 저장된 각 이름들 for구문 이용해 반복
             // 부원의 성 추출
-            String[] nameParts = name.split("\\s+");
-            String lastName = nameParts[0]; // 부원의 성 추출
-            if (lastName.startsWith(myLastName)) {
-                count++;
+            if (name.startsWith(inputMyName.substring(0,1))) { //부원의 이름(name)의 첫 글자가 inputMyName에 저장된 첫 글자와 같은지 확인
+                sameName.add(name); //같은 성을 가졌다면 추가시킴
+                count++; //+1증가
             }
         }
         System.out.println("총 " + count + "명입니다.");
