5ccdc76dcb25a9c07cb850b2f76fc99845050ba5
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-05-02 22:38:20 +0900

    3주차 과제 완료

--- 주찬/homework/Week03_EX01.java
+++ 주찬/homework/Week03_EX01.java
@@ -4,27 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 
 class Board {
-     String name;
-     int level;
-     int power;
+     String name; //이름을 받는 멤버변수
+     int level; // 레벨을 받는 멤버변수
+     int power; // 파워를 받는 멤버변수
 
 
-   public Board(String name, int level, int power) {
+   public Board(String name, int level, int power) { //생성자 선언 생성자는 클래스 이름이랑 동일
         this.name = name;
         this.level = level;
         this.power = power;
     }
 
     public String getName() {
-        return this.name;
+        return this.name; // 이름 반환
     }
 
     public int getLevel() {
-        return this.level;
+        return this.level; // 레벨 반환
     }
 
     public int getPower() {
-        return this.power;
+        return this.power; // 파워 반환
     }
 
     public static class Week03_EX01 {
@@ -36,7 +36,7 @@ class Board {
             ));
 
             System.out.println("[user name]");
-            boards.stream().map(Board::getName).forEach(System.out::println);
+            boards.stream().map(Board::getName).forEach(System.out::println); //리스트를 스트림으로 변환 후 이름을 받아와 매핑한뒤 출력
             System.out.println();
 
             System.out.println("[user power 합계]");
@@ -46,7 +46,7 @@ class Board {
             
             System.out.println("[user level 15 이상]");
             boards.stream()
-                    .filter(t -> t.getLevel() >= 15)
+                    .filter(t -> t.getLevel() >= 15) //t 는 각 요소를 나타내는 변수
                     .map(Board::getName)
                     .map(String::toUpperCase) //대문자 전환
                     .sorted() //정렬
