03c78f020d2b21514a65f1240c79ca431ea08e4b
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-05-09 14:28:27 +0900

    5주차 멘토링

--- 주찬/homework/Mentoring05_EX02.java
+++ 주찬/homework/Mentoring05_EX02.java
@@ -3,17 +3,17 @@ package homework;
 public class Mentoring05_EX02 {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i <=5; i++) { //1~5까지
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = 4; i > 0; i--) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 4; i > 0; i--) { // 별 개수
+            for (int j = 1; j <= i; j++) { // j가 i보다 작거나 같을때만 * 출력
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(); // 줄 띄우기
         }
     }
 }
