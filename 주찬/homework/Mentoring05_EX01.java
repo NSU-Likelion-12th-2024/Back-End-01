70b82ea028716311985852411689bf0589048cd0
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-05-09 14:28:55 +0900

    5주차 멘토링

--- 주찬/homework/Mentoring05_EX01.java
+++ 주찬/homework/Mentoring05_EX01.java
@@ -2,10 +2,14 @@ package homework;
 
 import java.util.Scanner;
 
+
 interface Caculateor {
     int add(int a, int b);
 
     int subtract(int a, int b);
+
+    void for_gugu(int a);
+
 }
 
 class CauculatorSet implements Caculateor {
@@ -19,6 +23,15 @@ class CauculatorSet implements Caculateor {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    @Override
+    public void for_gugu(int a) {
+        for (int i = 1; i <= 9; i++) {
+            int result = a * i;
+            System.out.println(a+" x "+i+" = "+ result);
+        }
+
+    }
 }
 
 public class Mentoring05_EX01 {
@@ -26,31 +39,29 @@ public class Mentoring05_EX01 {
         CauculatorSet cauculatorSet = new CauculatorSet();
         Scanner scanner = new Scanner(System.in);
         System.out.println("메뉴 : 1.덧셈 2.뺄셈");
-        System.out.printf("메뉴 선택:"+" ");
+        System.out.printf("메뉴 선택: ");
         int menu = scanner.nextInt();
 
         if (menu == 1) {
-            System.out.printf("두 수 입력:"+" "); //스캐너 입력을 옆에 바로 하기 위해서 printf 사용
+            System.out.printf("두 수 입력: "); //스캐너 입력을 옆에 바로 하기 위해서 printf 사용
             int num1 = scanner.nextInt();
             int num2 = scanner.nextInt();
             int add = cauculatorSet.add(num1, num2);
-            System.out.println("결과값:" +" "+ add);
-            for (int i = 1; i <= 9; i++) {
-                int multiplicationa1 = i * add;
-                System.out.println(add +" "+"x"+" "+i+" "+"="+" "+ multiplicationa1); //사이사이 공백 추가
-            }
+            System.out.println("결과값: " + add);
+            cauculatorSet.for_gugu(add); //CauculatorSet의 인스턴스인 cauculatorSet의 for_gugu에 add를 넣음
+
         } else if (menu == 2) {
-            System.out.printf("두 수 입력:"+" ");
+            System.out.printf("두 수 입력: ");
             int num1 = scanner.nextInt();
             int num2 = scanner.nextInt();
             int subtract = cauculatorSet.subtract(num1, num2);
-            System.out.println("결과값:" + " " + subtract);
-            for (int i = 1; i <= 9; i++) {
-                int multiplicationa2 = subtract - i;
-                System.out.println(subtract +" "+"x"+" "+i+" "+"="+" "+ multiplicationa2);
-            }
+            System.out.println("결과값: " + +subtract);
+            cauculatorSet.for_gugu(subtract); //CauculatorSet의 인스턴스인 cauculatorSet의 for_gugu에 subtract를 넣음
         } else {
             System.out.println("메뉴가 없습니다.");
+
         }
-    }
+        }
+
 }
+
